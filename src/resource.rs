99c74db2427b9851bb7cda553e62a06ed68cbeb1
Dawdaborje <dawdaborjekujabi@gmail.com>
2026-03-17 09:30:47 +0000

    Updating...

--- src/resource.rs
+++
@@ -1,9 +0,0 @@
-use serde_json::Value;
-
-pub trait ResourceImpl {
-    pub fn build_r4_resource(data: Value);
-    pub fn build_r4b_resource(data: Value);
-    pub fn build_r4b_resource(data: Value);
-    pub fn build_r5_resource(data: Value);
-    pub fn build_r6_resource(data: Value);
-}
